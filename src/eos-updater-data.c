838701ee34eaeddd8dcc51541f310774b2e13498
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-04-19 11:52:10 +0200

    Add a struct to pass it around
    We will use it to pass it into callbacks instead of OstreeRepo, so we
    can later add some more members if there is a need.

---
+++ src/eos-updater-data.c
@@ -0,0 +1,45 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright 2016 Kinvolk GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Krzesimir Nowak <krzesimir@kinvolk.io>
+ */
+
+#include "eos-updater-data.h"
+
+#include <string.h>
+
+void
+eos_updater_data_init (EosUpdaterData *data,
+                       OstreeRepo *repo)
+{
+  g_return_if_fail (data == NULL);
+  g_return_if_fail (OSTREE_IS_REPO (repo));
+
+  memset (data, 0, sizeof *data);
+  data->repo = g_object_ref (repo);
+}
+
+void
+eos_updater_data_clear (EosUpdaterData *data)
+{
+  if (data == NULL)
+    return;
+
+  g_clear_object (&data->repo);
+}
---
+++ src/eos-updater-data.h
@@ -0,0 +1,41 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright 2016 Kinvolk GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Krzesimir Nowak <krzesimir@kinvolk.io>
+ */
+
+#pragma once
+
+#include <ostree.h>
+
+G_BEGIN_DECLS
+
+typedef struct EosUpdaterData EosUpdaterData;
+
+struct EosUpdaterData
+{
+  OstreeRepo *repo;
+};
+
+void eos_updater_data_init (EosUpdaterData *data,
+                            OstreeRepo *repo);
+
+void eos_updater_data_clear (EosUpdaterData *data);
+
+G_END_DECLS
