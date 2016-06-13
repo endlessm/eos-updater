27986eaeefd7c53f35e5a497f7aa2daa40494f68
Krzesimir Nowak <krzesimir@kinvolk.io>
2016-06-13 14:25:21 +0200

    Add a set of poll-specific functions and objects
    Polling for updates usually means getting the list of update sources,
    getting the latest branch configuration data from them, getting an
    OSTree ref specific for the hardware from the branch configuration
    data and then getting the checksum of the commit being an update from
    the OSTree ref together with a list of sources this commit can be
    downloaded from. After all this, the updater sends the metrics
    informations.
    
    This file contains functions that do some of the aforementioned
    actions like getting hardware information, parsing the branch
    configuration data, fetching the latest commit and checking if the
    commit is an update.
    
    All other actions are specific to the poller and will have to be
    implemented by themselves.

---
+++ src/eos-updater-poll-common.h
@@ -0,0 +1,138 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright 2016 Kinvolk GmbH
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * Author: Krzesimir Nowak <krzesimir@kinvolk.io>
+ */
+
+#pragma once
+
+#include "eos-updater-branch-file.h"
+#include "eos-updater-data.h"
+#include "eos-updater-extensions.h"
+
+#include "eos-refcounted.h"
+
+#include <ostree.h>
+
+#include <glib.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+gboolean
+is_checksum_an_update (OstreeRepo *repo,
+                       const gchar *checksum,
+                       GVariant **commit,
+                       GError **error);
+
+#define EOS_TYPE_METRICS_INFO eos_metrics_info_get_type ()
+EOS_DECLARE_REFCOUNTED (EosMetricsInfo,
+                        eos_metrics_info,
+                        EOS,
+                        METRICS_INFO)
+
+struct _EosMetricsInfo
+{
+  GObject parent_instance;
+
+  gchar *vendor;
+  gchar *product;
+  gchar *ref;
+  gboolean on_hold;
+  EosBranchFile *branch_file;
+};
+
+#define EOS_TYPE_UPDATE_INFO eos_update_info_get_type ()
+EOS_DECLARE_REFCOUNTED (EosUpdateInfo,
+                        eos_update_info,
+                        EOS,
+                        UPDATE_INFO)
+
+struct _EosUpdateInfo
+{
+  GObject parent_instance;
+
+  gchar *checksum;
+  GVariant *commit;
+  gchar *refspec;
+  gchar *original_refspec;
+  gchar **urls;
+  EosExtensions *extensions;
+};
+
+EosUpdateInfo *
+eos_update_info_new (const gchar *csum,
+                     GVariant *commit,
+                     const gchar *refspec,
+                     const gchar *original_refspec,
+                     gchar **urls,
+                     EosExtensions *extensions);
+
+#define EOS_TYPE_METADATA_FETCH_DATA eos_metadata_fetch_data_get_type ()
+EOS_DECLARE_REFCOUNTED (EosMetadataFetchData,
+                        eos_metadata_fetch_data,
+                        EOS,
+                        METADATA_FETCH_DATA)
+
+struct _EosMetadataFetchData
+{
+  GObject parent_instance;
+
+  GTask *task;
+  EosUpdaterData *data;
+  GMainContext *context;
+};
+
+EosMetadataFetchData *
+eos_metadata_fetch_data_new (GTask *task,
+                             EosUpdaterData *data,
+                             GMainContext *context);
+
+typedef gboolean (*MetadataFetcher) (EosMetadataFetchData *fetch_data,
+                                     EosUpdateInfo **info,
+                                     EosMetricsInfo **metrics,
+                                     GError **error);
+
+gboolean get_upgrade_info_from_branch_file (EosBranchFile *branch_file,
+                                            gchar **upgrade_refspec,
+                                            gchar **original_refspec,
+                                            EosMetricsInfo **metrics,
+                                            GError **error);
+
+gboolean fetch_latest_commit (OstreeRepo *repo,
+                              GCancellable *cancellable,
+                              const gchar *remote_name,
+                              const gchar *ref,
+                              const gchar *url_override,
+                              gchar **out_checksum,
+                              EosExtensions **out_extensions,
+                              GError **error);
+
+gboolean download_file_and_signature (const gchar *url,
+                                      GBytes **contents,
+                                      GBytes **signature,
+                                      GError **error);
+
+gboolean get_origin_refspec (OstreeDeployment *booted_deployment,
+                             gchar **out_refspec,
+                             GError **error);
+
+GHashTable *get_hw_descriptors (void);
+
+G_END_DECLS
