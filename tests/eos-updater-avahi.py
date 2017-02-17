8b7267636ff8de6ddbfcdb5d577cb9d83506b85c
Philip Withnall <withnall@endlessm.com>
2017-02-17 17:05:08 +0000

    tests: Add basic integration test for eos-updater-avahi
    This is an installed-only integration test which pokes the actual
    configuration files on a deployed system to see if the Avahi service
    advertisement changes accordingly.
    
    Only a single test case is included in the test at the moment — this
    commit is mostly about adding the test framework and build/packaging
    integration.
    
    This adds a MIT-licensed file from pycotap: taptestrunner.py. Its
    inclusion is compatible with its MIT license. We could alternatively not
    copy it into our code base; that would require packaging pycotap and
    adding a dependency on it. This seems like a bit of a waste of time for
    ~180 lines of Python.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T15544

---
+++ tests/eos-updater-avahi.py
@@ -0,0 +1,167 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Copyright © 2017 Endless Mobile, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+"""Integration tests for Avahi .service file updates."""
+
+import os
+import subprocess
+import time
+import unittest
+
+from gi.repository import GLib
+import taptestrunner
+
+
+class TestEosUpdaterAvahi(unittest.TestCase):
+    """Integration test for Avahi .service file updates.
+
+    It can only be run when installed, due to requiring all the systemd unit
+    files to be in place.
+
+    It must be run as root in order to be able to modify all the files it needs
+    to touch.
+    """
+
+    def setUp(self):
+        self.timeout_seconds = 10  # seconds per test
+        self._main_context = GLib.main_context_default()
+        self.__timed_out = False
+        self.__service_file = '/etc/avahi/services/eos-updater.service'
+        self.__config_file = '/etc/eos-updater/eos-update-server.conf'
+
+    def tearDown(self):
+        self.assertFalse(self._main_context.iteration(False))
+        self._main_context = None
+
+        try:
+            os.unlink(self.__service_file)
+        except OSError:
+            pass
+        try:
+            os.unlink(self.__config_file)
+        except OSError:
+            pass
+
+        # Explicitly stop the service so it isn’t running when the next test
+        # starts, which could confuse things.
+        subprocess.check_call(['systemctl', 'stop', '--quiet',
+                               'eos-updater-avahi.service'])
+
+        # And wait for a second to avoid triggering systemd’s rate limiting
+        # for start requests.
+        time.sleep(1)
+
+    def __timeout_cb(self):
+        """Callback when a timeout is reached on a long-running operation."""
+        self.__timed_out = True
+        self._main_context.wakeup()
+
+        return GLib.SOURCE_REMOVE
+
+    def _wait_for_condition(self, condition_function):
+        """Block until condition_function returns True, or a timeout occurs."""
+        timeout_id = GLib.timeout_add_seconds(self.timeout_seconds,
+                                              self.__timeout_cb)
+
+        while not condition_function(self) and not self.__timed_out:
+            self._main_context.iteration(False)
+
+        if self.__timed_out:
+            self.fail('Timeout reached')
+        else:
+            GLib.Source.remove(timeout_id)
+
+    def _is_service_enabled(self, unit_file):
+        out = subprocess.check_output(['systemctl', 'is-enabled', unit_file])
+        out = out.decode('utf-8').strip()
+        return out in ['enabled', 'enabled-runtime',
+                       'linked', 'linked-runtime']
+
+    def test_disabled_by_default(self):
+        """Test Avahi adverts are disabled by default, on a clean install."""
+        self.assertFalse(os.path.isfile(self.__service_file))
+
+    def test_enable_via_configuration_file(self):
+        """Test enabling the configuration file creates the .service file."""
+        os.makedirs('/etc/eos-updater/', mode=0o755, exist_ok=True)
+        with open(self.__config_file, 'w') as conf_file:
+            conf_file.write(
+                '[Local Network Updates]\n'
+                'AdvertiseUpdates=true\n'
+            )
+
+        self._wait_for_condition(
+            lambda s: os.path.isfile(self.__service_file))
+
+    def test_disable_via_configuration_file(self):
+        """Test disabling the configuration file deletes the .service file."""
+        os.makedirs('/etc/eos-updater/', mode=0o755, exist_ok=True)
+        with open(self.__config_file, 'w') as conf_file:
+            conf_file.write(
+                '[Local Network Updates]\n'
+                'AdvertiseUpdates=false\n'
+            )
+
+        self._wait_for_condition(
+            lambda s: not os.path.isfile(self.__service_file))
+
+    def test_update_on_ostree_update(self):
+        """Test updating the OSTree deployment updates the .service file."""
+        def get_mtime_if_exists(path):
+            try:
+                return os.stat(path).st_mtime
+            except FileNotFoundError:
+                return 0
+
+        # Enable advertisements first and wait for the service file to appear
+        # as a result.
+        os.makedirs('/etc/eos-updater/', mode=0o755, exist_ok=True)
+        with open(self.__config_file, 'w') as conf_file:
+            conf_file.write(
+                '[Local Network Updates]\n'
+                'AdvertiseUpdates=true\n'
+            )
+
+        self._wait_for_condition(
+            lambda s: os.path.isfile(self.__service_file))
+        before = get_mtime_if_exists(self.__service_file)
+
+        # Wait 2s to ensure the mtimes will change from after enabling
+        # advertisements.
+        time.sleep(2)
+
+        # `touch` a deployment ref
+        with os.fdopen(os.open('/ostree/repo/refs/heads/ostree/0/1/0',
+                               flags=os.O_CREAT | os.O_APPEND,
+                               mode=0o644)) as f:
+            os.utime(f.fileno(), None)
+
+        self._wait_for_condition(
+            lambda s: get_mtime_if_exists(self.__service_file) > before)
+
+    def test_services_enabled_by_default(self):
+        """Test the Avahi systemd units are enabled by default."""
+        self.assertTrue(self._is_service_enabled('eos-update-server.service'))
+        self.assertTrue(self._is_service_enabled('eos-updater-avahi.service'))
+        self.assertTrue(self._is_service_enabled('avahi-daemon.service'))
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=taptestrunner.TAPTestRunner())
