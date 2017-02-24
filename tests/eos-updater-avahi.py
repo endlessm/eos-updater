25626ef14a04e4adfc8b3f06b4ea8177c03a29f2
Philip Withnall <withnall@endlessm.com>
2017-02-24 16:35:33 +0000

    tests: Skip Python integration tests if not running as root
    All except one of the Python integration tests need to be run as root in
    order to manipulate the system sufficiently to test things. Skip them if
    not running as root, rather than failing part-way through the tests when
    we find we don’t have sufficient permissions.
    
    Signed-off-by: Philip Withnall <withnall@endlessm.com>
    
    https://phabricator.endlessm.com/T15544

--- tests/eos-updater-avahi.py
+++ tests/eos-updater-avahi.py
@@ -98,6 +98,7 @@ class TestEosUpdaterAvahi(unittest.TestCase):
         """Test Avahi adverts are disabled by default, on a clean install."""
         self.assertFalse(os.path.isfile(self.__service_file))
 
+    @unittest.skipIf(os.geteuid() != 0, "Must be run as root")
     def test_enable_via_configuration_file(self):
         """Test enabling the configuration file creates the .service file."""
         os.makedirs('/etc/eos-updater/', mode=0o755, exist_ok=True)
@@ -110,6 +111,7 @@ class TestEosUpdaterAvahi(unittest.TestCase):
         self._wait_for_condition(
             lambda s: os.path.isfile(self.__service_file))
 
+    @unittest.skipIf(os.geteuid() != 0, "Must be run as root")
     def test_disable_via_configuration_file(self):
         """Test disabling the configuration file deletes the .service file."""
         os.makedirs('/etc/eos-updater/', mode=0o755, exist_ok=True)
@@ -122,6 +124,7 @@ class TestEosUpdaterAvahi(unittest.TestCase):
         self._wait_for_condition(
             lambda s: not os.path.isfile(self.__service_file))
 
+    @unittest.skipIf(os.geteuid() != 0, "Must be run as root")
     def test_update_on_ostree_update(self):
         """Test updating the OSTree deployment updates the .service file."""
         def get_mtime_if_exists(path):
@@ -156,6 +159,7 @@ class TestEosUpdaterAvahi(unittest.TestCase):
         self._wait_for_condition(
             lambda s: get_mtime_if_exists(self.__service_file) > before)
 
+    @unittest.skipIf(os.geteuid() != 0, "Must be run as root")
     def test_services_enabled_by_default(self):
         """Test the Avahi systemd units are enabled by default."""
         self.assertTrue(self._is_service_enabled('eos-update-server.service'))
