a40921202d306b8c4eb67018275112476a48637d
Krzesimir Nowak <krzesimir@kinvolk.io>
2017-05-04 14:23:43 +0200

    util: Rewrite service file generation
    - It handles both text and binary values in TXT record pairs.
    - It returns an error if the length of the TXT record pair is greater
      than 255 bytes.
    - It does not hardcode the version field - it is a part of the passed
      TXT records GVariant.
    - It does not hardcode the name of the service - it is passed as one
      of the parameters to the generator function.
    
    Removing hardcoding of the name and the version field is for the
    another DNS-SD TXT records format coming in the following commits.

--- libeos-updater-util/avahi-service-file.c
+++ libeos-updater-util/avahi-service-file.c
@@ -37,67 +37,192 @@ const gchar * const EOS_UPDATER_AVAHI_SERVICE_TYPE = "_eos_updater._tcp";
 const gchar * const eos_avahi_v1_ostree_path = "eos_ostree_path";
 const gchar * const eos_avahi_v1_head_commit_timestamp = "eos_head_commit_timestamp";
 
-static gchar *
-txt_records_to_string (const gchar **txt_records)
+typedef enum
+  {
+    TXT_VALUE_TYPE_TEXT,
+    TXT_VALUE_TYPE_BINARY,
+  } TxtValueType;
+
+static TxtValueType
+classify_txt_value (GVariant *txt_value)
 {
+  gboolean txt_value_is_text = g_variant_is_of_type (txt_value,
+                                                     G_VARIANT_TYPE_STRING);
+  gboolean txt_value_is_binary = g_variant_is_of_type (txt_value,
+                                                       G_VARIANT_TYPE_BYTESTRING);
+
+  g_assert (txt_value_is_binary || txt_value_is_text);
+
+  if (txt_value_is_text)
+    return TXT_VALUE_TYPE_TEXT;
+
+  return TXT_VALUE_TYPE_BINARY;
+}
+
+static gboolean
+check_record_size (const gchar  *key,
+                   gsize         record_size,
+                   GError      **error)
+{
+  if (record_size > 255)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "the TXT record with key %s is longer than 255 bytes", key);
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+static gboolean
+handle_text_record (GString      *records_str,
+                    const gchar  *key,
+                    GVariant     *text_value,
+                    GError      **error)
+{
+  gsize value_string_length;
+  const gchar *value_string = g_variant_get_string (text_value,
+                                                    &value_string_length);
+  gsize key_length = strlen (key);
+  g_autofree gchar *escaped_key = NULL;
+  g_autofree gchar *escaped_value = NULL;
+  gsize record_size = value_string_length + key_length + 1;
+
+  if (!check_record_size (key, record_size, error))
+    return FALSE;
+
+  escaped_key = g_markup_escape_text (key, key_length);
+  escaped_value = g_markup_escape_text (value_string, value_string_length);
+  g_string_append_printf (records_str,
+                          "    <txt-record>%s=%s</txt-record>\n",
+                          escaped_key,
+                          escaped_value);
+  return TRUE;
+}
+
+static gboolean
+handle_binary_record (GString      *records_str,
+                      const gchar  *key,
+                      GVariant     *binary_value,
+                      GError      **error)
+{
+  gsize value_data_length;
+  gconstpointer value_data = g_variant_get_fixed_array (binary_value,
+                                                        &value_data_length,
+                                                        1);
+  gsize key_length = strlen (key);
+  g_autofree gchar *escaped_key = NULL;
+  g_autofree gchar *encoded_value = NULL;
+  g_autofree gchar *escaped_value = NULL;
+  gsize record_size = value_data_length + key_length + 1;
+
+  if (!check_record_size (key, record_size, error))
+    return FALSE;
+
+  escaped_key = g_markup_escape_text (key, key_length);
+  encoded_value = g_base64_encode (value_data, value_data_length);
+  escaped_value = g_markup_escape_text (encoded_value, -1);
+  g_string_append_printf (records_str,
+                          "    <txt-record value-format=\"binary-base64\">%s=%s</txt-record>\n",
+                          escaped_key,
+                          escaped_value);
+  return TRUE;
+}
+
+static gchar *
+txt_records_to_string (GVariant  *txt_records,
+                       GError   **error)
+{
+  GVariantIter iter;
   g_autoptr(GString) str = NULL;
-  gsize idx;
+  g_autoptr(GVariant) txt_value = NULL;
+  const gchar *txt_key;
+  gsize total_size = 0;
+
+  g_assert (g_variant_is_of_type (txt_records, G_VARIANT_TYPE ("a(sv)")));
 
   str = g_string_new ("");
-
-  for (idx = 0; txt_records[idx] != NULL; idx++)
+  g_variant_iter_init (&iter, txt_records);
+  while (g_variant_iter_loop (&iter, "(&sv)", &txt_key, &txt_value))
     {
-      g_autofree gchar *record_escaped = g_markup_escape_text (txt_records[idx], -1);
-      g_string_append_printf (str, "    <txt-record>%s</txt-record>\n",
-                              record_escaped);
+      TxtValueType txt_value_type = classify_txt_value (txt_value);
+
+      switch (txt_value_type)
+        {
+        case TXT_VALUE_TYPE_TEXT:
+          if (!handle_text_record (str, txt_key, txt_value, error))
+            return FALSE;
+          break;
+
+        case TXT_VALUE_TYPE_BINARY:
+          if (!handle_binary_record (str, txt_key, txt_value, error))
+            return FALSE;
+          break;
+
+        default:
+          g_assert_not_reached ();
+        }
     }
 
   return g_string_free (g_steal_pointer (&str), FALSE);
 }
 
 static GBytes *
-generate_from_avahi_service_template (const gchar *type,
-                                      guint16 port,
-                                      const gchar *txt_version,
-                                      const gchar **txt_records)
+generate_from_avahi_service_template (const gchar  *name,
+                                      const gchar  *type,
+                                      guint16       port,
+                                      GVariant     *txt_records,
+                                      GError      **error)
 {
   g_autofree gchar *service_group = NULL;
   gsize service_group_len;  /* bytes, not including trailing nul */
-  g_autofree gchar *txt_records_str = txt_records_to_string (txt_records);
-  g_autofree gchar *type_escaped = g_markup_escape_text (type, -1);
-  g_autofree gchar *txt_version_escaped = g_markup_escape_text (txt_version, -1);
+  g_autofree gchar *txt_records_str = txt_records_to_string (txt_records,
+                                                             error);
+  g_autofree gchar *type_escaped = NULL;
+  g_autofree gchar *name_escaped = NULL;
 
+  if (txt_records_str == NULL)
+    return NULL;
+
+  type_escaped = g_markup_escape_text (type, -1);
+  name_escaped = g_markup_escape_text (name, -1);
   service_group = g_strdup_printf (
       "<service-group>\n"
-      "  <name replace-wildcards=\"yes\">EOS update service on %%h</name>\n"
+      "  <name replace-wildcards=\"yes\">%s</name>\n"
       "  <service>\n"
       "    <type>%s</type>\n"
       "    <port>%" G_GUINT16_FORMAT "</port>\n"
-      "    <txt-record>eos_txt_version=%s</txt-record>\n"
       "%s"
       "  </service>\n"
       "</service-group>\n",
-      type_escaped, port, txt_version_escaped, txt_records_str);
+      name_escaped, type_escaped, port, txt_records_str);
   service_group_len = strlen (service_group);
 
   return g_bytes_new_take (g_steal_pointer (&service_group), service_group_len);
 }
 
 static gboolean
-generate_avahi_service_template_to_file (GFile *path,
-                                         const gchar *txt_version,
-                                         const gchar **txt_records,
-                                         GCancellable *cancellable,
-                                         GError **error)
+generate_avahi_service_template_to_file (GFile         *path,
+                                         const gchar   *name,
+                                         const gchar   *type,
+                                         guint16        port,
+                                         GVariant      *txt_records,
+                                         GCancellable  *cancellable,
+                                         GError       **error)
 {
   g_autoptr(GBytes) contents = NULL;
   gconstpointer raw;
   gsize raw_len;
+  g_autoptr(GVariant) reffed_txt_records = g_variant_ref_sink (txt_records);
 
-  contents = generate_from_avahi_service_template (EOS_UPDATER_AVAHI_SERVICE_TYPE,
-                                                   EOS_AVAHI_PORT,
-                                                   txt_version,
-                                                   txt_records);
+  contents = generate_from_avahi_service_template (name,
+                                                   type,
+                                                   port,
+                                                   reffed_txt_records,
+                                                   error);
+  if (contents == NULL)
+    return FALSE;
+
   raw = g_bytes_get_data (contents, &raw_len);
   return g_file_replace_contents (path,
                                   raw,
@@ -110,35 +235,33 @@ generate_avahi_service_template_to_file (GFile *path,
                                   error);
 }
 
-static gchar *
-txt_record (const gchar *key,
-            const gchar *value)
-{
-  return g_strdup_printf ("%s=%s", key, value);
-}
-
 static gboolean
-generate_v1_service_file (const gchar *ostree_path,
-                          GDateTime *head_commit_timestamp,
-                          GFile *service_file,
-                          GCancellable *cancellable,
-                          GError **error)
+generate_v1_service_file (const gchar   *ostree_path,
+                          GDateTime     *head_commit_timestamp,
+                          GFile         *service_file,
+                          GCancellable  *cancellable,
+                          GError       **error)
 {
-  g_autoptr(GPtrArray) txt_records = NULL;
   g_autofree gchar *timestamp_str = NULL;
+  const GVariantType *records_type = G_VARIANT_TYPE ("a(sv)");
+  g_auto(GVariantBuilder) records_builder = G_VARIANT_BUILDER_INIT (records_type);
 
   timestamp_str = g_date_time_format (head_commit_timestamp, "%s");
-  txt_records = g_ptr_array_new_with_free_func (g_free);
+  g_variant_builder_add (&records_builder, "(sv)",
+                         "eos_txt_version",
+                         g_variant_new_string ("1"));
+  g_variant_builder_add (&records_builder, "(sv)",
+                         eos_avahi_v1_ostree_path,
+                         g_variant_new_string (ostree_path));
+  g_variant_builder_add (&records_builder, "(sv)",
+                         eos_avahi_v1_head_commit_timestamp,
+                         g_variant_new_string (timestamp_str));
 
-  g_ptr_array_add (txt_records, txt_record (eos_avahi_v1_ostree_path,
-                                            ostree_path));
-  g_ptr_array_add (txt_records, txt_record (eos_avahi_v1_head_commit_timestamp,
-                                            timestamp_str));
-
-  g_ptr_array_add (txt_records, NULL);
   return generate_avahi_service_template_to_file (service_file,
-                                                  "1",
-                                                  (const gchar **)txt_records->pdata,
+                                                  "EOS update service on %h",
+                                                  EOS_UPDATER_AVAHI_SERVICE_TYPE,
+                                                  EOS_AVAHI_PORT,
+                                                  g_variant_builder_end (&records_builder),
                                                   cancellable,
                                                   error);
 }
@@ -200,11 +323,11 @@ get_service_file (const gchar *avahi_service_directory)
  * Returns: %TRUE on success, %FALSE otherwise
  */
 gboolean
-eos_avahi_service_file_generate (const gchar *avahi_service_directory,
-                                 const gchar *ostree_path,
-                                 GDateTime *head_commit_timestamp,
-                                 GCancellable *cancellable,
-                                 GError **error)
+eos_avahi_service_file_generate (const gchar   *avahi_service_directory,
+                                 const gchar   *ostree_path,
+                                 GDateTime     *head_commit_timestamp,
+                                 GCancellable  *cancellable,
+                                 GError       **error)
 {
   g_autoptr(GFile) service_file = NULL;
 
