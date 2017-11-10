3b230f0b54ee27fd9c98f3e9add72c30f4c524ac
Sam Spilsbury <sam@endlessm.com>
2017-11-10 17:43:49 +0800

    fixup! apply: Add a mechanism to atomically update flatpaks with OS upgrades
    --
    
    Add documentation for EosUpdaterInstallerMode

--- eos-updater-flatpak-installer/installer.h
+++ eos-updater-flatpak-installer/installer.h
@@ -26,6 +26,29 @@
 
 G_BEGIN_DECLS
 
+/**
+ * EosUpdaterInstallerMode:
+ * @EU_INSTALLER_MODE_PERFORM: Actually perform actions in the installer
+ *                             installing or uninstalling flatpaks as necessary,
+ *                             this is the default mode.
+ * @EU_INSTALLER_MODE_STAMP: Just update the counter files to the most
+ *                           up-to-date counter for each of the auto-install
+ *                           files but don't perform actions. This is typically
+ *                           used by the image builder to keep the auto-install
+ *                           state in sync with the installed flatpaks.
+ * @EU_INSTALLER_MODE_CHECK: Check that flatpak ref actions up to a
+ *                           certain serial number are applied on the system,
+ *                           that is that all flatpaks that should have been
+ *                           installed are installed and all flatpaks that
+ *                           should have been uninstalled are not installed.
+ *                           Note that this mode is not useful as a debugging
+ *                           tool, because a user can legitimately uninstall
+ *                           or install flatpaks of the same name after
+ *                           an update has occurred.
+ *
+ * Enum values used to specify the mode that the flatpak-installer runs in
+ *
+ */
 typedef enum {
   EU_INSTALLER_MODE_PERFORM = 0,
   EU_INSTALLER_MODE_STAMP = 1,
