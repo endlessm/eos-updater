7c8e13b5eab909489d9b0cd5c54107057da5224e
Sam Spilsbury <sam@endlessm.com>
2017-12-07 15:27:00 +0800

    installer: Add eos-updater-flatpak-installer
    This binary uses the functions in libeos-updater-flatpak-installer
    to actually install flatpaks on reboot. It also has a "check" mode
    which checks if the flatpak actions we claim to have applied are actually
    fully-up-to-date on the system.
    
    (Modified by Philip Withnall to improve formatting on man pages and fix
    a few minor bugs found during review.)
    
    https://phabricator.endlessm.com/T16682

---
+++ eos-updater-flatpak-installer/main.c
@@ -0,0 +1,264 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright © 2017 Endless Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Authors:
+ *  - Sam Spilsbury <sam@endlessm.com>
+ */
+
+#include <flatpak.h>
+#include <glib.h>
+#include <libeos-updater-util/enums.h>
+#include <libeos-updater-util/flatpak.h>
+#include <libeos-updater-util/types.h>
+#include <libeos-updater-util/util.h>
+#include <libeos-updater-flatpak-installer/installer.h>
+#include <locale.h>
+#include <ostree.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+
+/* main() exit codes. */
+enum
+{
+  EXIT_OK = EXIT_SUCCESS,
+  EXIT_FAILED = 1,
+  EXIT_INVALID_ARGUMENTS = 2,
+  EXIT_CHECK_FAILED = 3,
+  EXIT_APPLY_FAILED = 4,
+};
+
+static int
+usage (GOptionContext *context,
+       const gchar    *error_message,
+       ...) G_GNUC_PRINTF (2, 3);
+
+static int
+usage (GOptionContext *context,
+       const gchar    *error_message,
+       ...)
+{
+  va_list ap;
+  g_autofree gchar *formatted_message = NULL;
+  g_autofree gchar *help = NULL;
+
+  /* Format the arguments. */
+  va_start (ap, error_message);
+  formatted_message = g_strdup_vprintf (error_message, ap);
+  va_end (ap);
+
+  /* Include the usage. */
+  help = g_option_context_get_help (context, TRUE, NULL);
+  g_printerr ("%s: %s\n\n%s\n", g_get_prgname (), formatted_message, help);
+
+  return EXIT_INVALID_ARGUMENTS;
+}
+
+static gboolean
+parse_installer_mode (const gchar              *mode,
+                      EosUpdaterInstallerMode  *out_mode,
+                      GError                  **error)
+{
+  GEnumClass *enum_class = g_type_class_ref (EOS_TYPE_UPDATER_INSTALLER_MODE);
+  GEnumValue *enum_value = g_enum_get_value_by_nick (enum_class, mode);
+
+  g_type_class_unref (enum_class);
+
+  if (enum_value == NULL)
+    {
+      g_set_error (error, G_IO_ERROR, G_IO_ERROR_FAILED,
+                   "Invalid installer mode ‘%s’", mode);
+      return FALSE;
+    }
+
+  *out_mode = (EosUpdaterInstallerMode) enum_value->value;
+
+  return TRUE;
+}
+
+static int
+fail (int          exit_status,
+      const gchar *error_message,
+      ...) G_GNUC_PRINTF (2, 3);
+
+static int
+fail (int          exit_status,
+      const gchar *error_message,
+      ...)
+{
+  va_list ap;
+  g_autofree gchar *formatted_message = NULL;
+
+  g_return_val_if_fail (exit_status > 0, EXIT_FAILED);
+
+  /* Format the arguments. */
+  va_start (ap, error_message);
+  formatted_message = g_strdup_vprintf (error_message, ap);
+  va_end (ap);
+
+  /* Include the usage. */
+  g_printerr ("%s: %s\n", g_get_prgname (), formatted_message);
+
+  return exit_status;
+}
+
+int
+main (int    argc,
+      char **argv)
+{
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GOptionContext) context = NULL;
+  g_autoptr(GFile) flatpaks_to_export_file = NULL;
+  g_autoptr(GHashTable) flatpak_ref_actions_for_this_boot = NULL;
+  g_autoptr(GHashTable) flatpak_ref_actions_progress = NULL;
+  g_autoptr(FlatpakInstallation) installation = NULL;
+  g_autofree gchar *formatted_flatpak_ref_actions_for_this_boot = NULL;
+  g_autofree gchar *formatted_flatpak_ref_actions_progress_for_this_boot = NULL;
+  const gchar *resolved_mode = NULL;
+  const gchar *pending_flatpak_deployments_state_path = euu_pending_flatpak_deployments_state_path ();
+  EosUpdaterInstallerMode parsed_mode;
+
+  g_autofree gchar *mode = NULL;
+  gboolean also_pull = FALSE;
+  GOptionEntry entries[] =
+    {
+      { "mode", 'm', 0, G_OPTION_ARG_STRING, &mode, "Mode to use (perform, stamp, check) (default: perform)", NULL },
+      { "pull", 'p', 0, G_OPTION_ARG_NONE, &also_pull, "Also pull flatpaks", NULL },
+      { NULL }
+    };
+
+  setlocale (LC_ALL, "");
+
+  context = g_option_context_new ("— Endless OS Updater Flatpak Installer");
+  g_option_context_add_main_entries (context, entries, NULL);
+  g_option_context_set_summary (context,
+                                "Install flatpak packages on system boot");
+
+  if (!g_option_context_parse (context, &argc, &argv, &error))
+    return usage (context, "Failed to parse options: %s", error->message);
+
+  resolved_mode = mode != NULL ? mode : "perform";
+
+  if (!parse_installer_mode (resolved_mode,
+                             &parsed_mode,
+                             &error))
+    return fail (EXIT_INVALID_ARGUMENTS, "%s", error->message);
+
+  installation = eos_updater_get_flatpak_installation (NULL, &error);
+
+  if (installation == NULL)
+    return fail (EXIT_FAILED, "Could not get flatpak installation: %s", error->message);
+
+  g_message ("Running in mode ‘%s’", resolved_mode);
+
+  if (also_pull)
+    g_message ("Will pull flatpaks as well as deploying them");
+
+  /* Check mode is completely different — we need to read in the action
+   * application state and check if there’s a delta between what we expect
+   * and what we have.
+   *
+   * Note that on a user system it might be perfectly legitimate for there
+   * to be a delta, because the user might have uninstalled or installed
+   * an app we marked as auto-install or auto-uninstall. Generally speaking
+   * you would use this mode on the image builder to catch situations where
+   * the apps list is out of sync.
+   */
+  switch (parsed_mode)
+    {
+      case EU_INSTALLER_MODE_CHECK:
+        {
+          g_autoptr(GHashTable) flatpak_ref_actions_to_check = NULL;
+          g_autoptr(GPtrArray) squashed_ref_actions_to_check = NULL;
+          g_autofree gchar *formatted_flatpak_ref_actions_to_check = NULL;
+          g_autofree gchar *formatted_ordered_flatpak_ref_actions_to_check = NULL;
+
+          flatpak_ref_actions_to_check = eufi_determine_flatpak_ref_actions_to_check (NULL, &error);
+
+          if (flatpak_ref_actions_to_check == NULL)
+            return fail (EXIT_FAILED, 
+                         "Could not get information on which flatpak ref actions to check: %s",
+                         error->message);
+
+          squashed_ref_actions_to_check = euu_flatten_flatpak_ref_actions_table (flatpak_ref_actions_to_check);
+
+          formatted_flatpak_ref_actions_to_check =
+            euu_format_all_flatpak_ref_actions ("All flatpak ref actions that should have been applied",
+                                                flatpak_ref_actions_to_check);
+          g_message ("%s", formatted_flatpak_ref_actions_to_check);
+
+          formatted_ordered_flatpak_ref_actions_to_check =
+            euu_format_flatpak_ref_actions_array ("Order in which actions will be checked",
+                                                  squashed_ref_actions_to_check);
+          g_message ("%s", formatted_ordered_flatpak_ref_actions_to_check);
+
+          if (!eufi_check_ref_actions_applied (installation,
+                                               pending_flatpak_deployments_state_path,
+                                               squashed_ref_actions_to_check,
+                                               &error))
+            return fail (EXIT_CHECK_FAILED,
+                         "Flatpak ref actions are not up to date: %s",
+                         error->message);
+
+          break;
+        }
+      case EU_INSTALLER_MODE_PERFORM:
+      case EU_INSTALLER_MODE_STAMP:
+        {
+          g_autoptr(GHashTable) new_flatpak_ref_actions_to_apply = NULL;
+          g_autoptr(GPtrArray) squashed_ref_actions_to_apply = NULL;
+          g_autofree gchar *formatted_flatpak_ref_actions_to_apply = NULL;
+          g_autofree gchar *formatted_ordered_flatpak_ref_actions_to_apply = NULL;
+
+          new_flatpak_ref_actions_to_apply = eufi_determine_flatpak_ref_actions_to_apply (NULL, &error);
+
+          if (new_flatpak_ref_actions_to_apply == NULL)
+            return fail (EXIT_FAILED,
+                         "Could not get information on which flatpak ref actions to check: %s",
+                         error->message);
+
+          squashed_ref_actions_to_apply = euu_flatten_flatpak_ref_actions_table (new_flatpak_ref_actions_to_apply);
+
+          formatted_flatpak_ref_actions_to_apply =
+            euu_format_all_flatpak_ref_actions ("All flatpak ref actions that are not yet applied",
+                                                new_flatpak_ref_actions_to_apply);
+          g_message ("%s", formatted_flatpak_ref_actions_to_apply);
+
+          formatted_ordered_flatpak_ref_actions_to_apply =
+            euu_format_flatpak_ref_actions_array ("Order in which actions will be applied",
+                                                  squashed_ref_actions_to_apply);
+          g_message ("%s", formatted_ordered_flatpak_ref_actions_to_apply);
+
+          if (!eufi_apply_flatpak_ref_actions (installation,
+                                               euu_pending_flatpak_deployments_state_path (),
+                                               squashed_ref_actions_to_apply,
+                                               parsed_mode,
+                                               also_pull ? EU_INSTALLER_FLAGS_ALSO_PULL : EU_INSTALLER_FLAGS_NONE,
+                                               &error))
+            return fail (EXIT_APPLY_FAILED,
+                         "Couldn’t apply some flatpak update actions for this boot: %s",
+                         error->message);
+
+          break;
+        }
+      default:
+        g_assert_not_reached ();
+    }
+
+  return EXIT_OK;
+}
