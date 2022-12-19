5ac80dda8f611beb53ba65496698e2b5bbbb20ae
Dan Nicholson <dbn@endlessos.org>
2022-12-19 10:27:04 -0700

    test-common: Always force autoupdater fetching
    When mogwai is installed, eos-updater needs to avoid download scheduling
    during testing or it may not fetch the commit. Normally this is done by
    running the autoupdater with `--force-update`, but that option enables
    several autoupdater policies. When testing the autoupdater that can make
    it impossible to discern its behavior.
    
    Introduce a new `--force-fetch` option and always set it when running
    the autoupdater from the test suite. Testing of the eos-updater download
    scheduling behavior can be done outside the use of the autoupdater if
    desired.
    
    https://phabricator.endlessm.com/T34083
    (cherry picked from commit c83a55241774edac0236e7bf3ac53f212bbfaa95)

--- test-common/utils.c
+++ test-common/utils.c
@@ -3486,6 +3486,8 @@ spawn_autoupdater (GFile *stamps_dir,
   const gchar *argv[] =
     {
       eos_autoupdater_binary,
+      /* Always use --force-fetch to bypass download scheduler. */
+      "--force-fetch",
       force_update_flag,
       NULL
     };
